c1a16cd1a5791c67d13d4382f51866f55489cf1e
raulbarrue <raulbarrue@outlook.com>
2020-06-14 21:02:40 +0100

    created api draft

---
+++ api/app.py
@@ -0,0 +1,19 @@
+from joblib import load
+import os
+import flask
+
+#os.chdir("./api")
+pipeline = load('./pipeline_model/titanic_pipeline.joblib')
+
+
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+app.run()
