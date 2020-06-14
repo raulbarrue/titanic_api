7c01eea26d299399717f07cc57a8868816e9b3d0
raulbarrue <raulbarrue@outlook.com>
2020-06-14 23:29:50 +0100

    API working :)

--- api/app.py
+++ api/app.py
@@ -1,19 +1,38 @@
 from joblib import load
+import numpy as np
 import os
 import flask
+from flask import request, jsonify
+import titanic_functions as tfunc
 
 #os.chdir("./api")
-pipeline = load('./pipeline_model/titanic_pipeline.joblib')
-
-
 
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return '''<h1>Titanic API</h1>
+<p>A project for practising Restful API in Machine Learning models</p>'''
 
-app.run()
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/', methods=['GET', 'POST'])
+def make_prediction():
+    data = request.get_json()
+    X = tfunc.clean_df(data).values
+    print(X)
+
+    prediction = str(pipeline.predict(X)[0])
+    print(type(prediction))
+    print(prediction)
+
+    return prediction
+    
+
+if __name__ == '__main__':
+    pipeline = load('./pipeline_model/titanic_pipeline.joblib')
+
+
+app.run(debug=True)
