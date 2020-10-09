2611917f95f36309498f0444934f55b37c2df3dd
raulbarrue <raulbarrue@outlook.com>
2020-10-09 10:11:23 +0100

    Deployed

--- azure/model_folder/score.py
+++ azure/model_folder/score.py
@@ -6,12 +6,20 @@ import joblib
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'titanic-api-model.pkl')
+    global scaler
+    model_path = './model_folder/titanic-api-model.pkl'
     model = joblib.load(model_path)
 
+    scaler_path = './model_folder/titanic-api-scaler.pkl'
+    scaler = joblib.load(scaler_path)
+
 def run(data):
+
+    #ws = Workspace(subscription_id="fc1c5e68-95c2-4bce-9ff9-5bd8442fb921", resource_group="titanic-api", workspace_name="titanic-ws")
+
     try:
-        data = np.array(json.loads(data))
+        data = json.loads(data)['data']
+        data = scaler.transform([data])
         result = model.predict(data)
         # You can return any data type, as long as it is JSON serializable.
         return result.tolist()
