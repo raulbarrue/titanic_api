b97e9cd35709b2ebe890e6796cd48f586dc4288c
raulbarrue <raulbarrue@outlook.com>
2020-10-10 19:26:01 +0100

    Stuff

--- azure/model_folder/score.py
+++ azure/model_folder/score.py
@@ -13,6 +13,25 @@ def init():
     scaler_path = './model_folder/titanic-api-scaler.pkl'
     scaler = joblib.load(scaler_path)
 
+###### POWER BI ######
+
+# providing 3 sample inputs for schema generation
+standard_sample_input = StandardPythonParameterType([1, "Raul", "male", 25, 0, 0, 300, "C32", "S"])
+
+# This is a nested input sample, any item wrapped by `ParameterType` will be described by schema
+sample_input = StandardPythonParameterType({'input1': standard_sample_input})
+
+#sample_global_parameters = StandardPythonParameterType(1.0) #this is optional
+sample_output = StandardPythonParameterType([1.0])
+
+@input_schema('inputs', sample_input)
+#@input_schema('global_parameters', sample_global_parameters) #this is optional
+@output_schema(sample_output)
+
+
+######################
+
+
 def run(data):
 
     #ws = Workspace(subscription_id="fc1c5e68-95c2-4bce-9ff9-5bd8442fb921", resource_group="titanic-api", workspace_name="titanic-ws")
